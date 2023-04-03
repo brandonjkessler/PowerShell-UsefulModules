1890ff54ff00148492e7cffa5a1f825f18ab6d89
Brandon Kessler <brandon.kessler@state.co.us>
2023-04-03 15:48:35 -0600

    Removes Version Folders.

---
+++ ModuleManifest.ps1
@@ -0,0 +1,5 @@
+#-- To Do: Create module manifests automatically
+#-- Look at each folder as the module name
+#-- Verify teach PSD1 matches the folder name
+#-- pull the version info from the sub folder
+#-- match psd1 to psm1
